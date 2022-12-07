c326fc674a8cd68f0a42eecb641c8e2a8cf4c9cc
jtorregrosa <jtorregrosalloret@gmail.com>
2022-12-07 13:39:15 +0100

    Fix configuration

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1219,7 +1219,7 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-//#define CLASSIC_JERK
+#define CLASSIC_JERK
 #if ENABLED(CLASSIC_JERK)
   #define DEFAULT_XJERK 10.0
   #define DEFAULT_YJERK 10.0
@@ -1706,7 +1706,7 @@
 #define Y_MIN_POS -12
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE + 12
+#define Y_MAX_POS Y_BED_SIZE + 11
 #define Z_MAX_POS 250
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
